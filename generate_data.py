afa33ad05273b970d854582c262294e32e0e0f31
Justin Sayah <55928969+justin-sayah@users.noreply.github.com>
2021-11-13 17:36:26 -0500

    finished data generation

--- generate_data.py
+++ generate_data.py
@@ -6,21 +6,35 @@ x = []
 y = []
 z = []
 w = []
+cereal = []
+hotdog = []
+sleep = []
+ice_cream = []
+messy = []
+aliens = []
+
 names = []
 fake = Faker()
 for _ in range(300):
   names.append(fake.name())
-  x.append(np.random.randint(0, 10))
-  y.append(np.random.randint(0, 10))
-  z.append(np.random.randint(0, 10))
-  w.append(np.random.randint(0, 10))
+  x.append(np.round(np.random.uniform(1, 10),2))
+  y.append(np.round(np.random.uniform(1, 10),2))
+  z.append(np.round(np.random.uniform(1, 10),2))
+  w.append(np.round(np.random.uniform(1, 10),2))
+  cereal.append(np.random.randint(3))
+  hotdog.append(np.random.randint(3))
+  sleep.append(np.random.randint(3))
+  ice_cream.append(np.random.randint(3))
+  messy.append(np.random.randint(3))
+  aliens.append(np.random.randint(3))
 
 
-data = {"Extrovert - Introvert": x, "Sensing - Intuition": y, "Thinking - Feeling": z, "Judging - Perceiving": w}
+data = {"Extrovert - Introvert": x, "Sensing - Intuition": y, "Thinking - Feeling": z, "Judging - Perceiving": w, "Cereal": cereal,
+ "Hotdog":hotdog, "Sleep": sleep, "Ice Cream": ice_cream, "Messy": messy, "Aliens":aliens}
 df = pd.DataFrame(data)
 
-neigh = AgglomerativeClustering(n_neighbors=3)
-clustering = AgglomerativeClustering().fit(data)
+# neigh = AgglomerativeClustering(n_neighbors=3)
+# clustering = AgglomerativeClustering().fit(data)
 
 
 print(df)
