a285be83aabee80c917a01f5b397eec7e8c6fe5a
PatKuz <pkuzdzal@bu.edu>
2021-11-14 04:23:26 -0500

    dis da big change :O

--- generate_human.py
+++ generate_human.py
@@ -4,6 +4,8 @@ import PIL.Image as img
 def make_human():
     obj = img.open(io.BytesIO(requests.get("https://thispersondoesnotexist.com/image", headers={'User-Agent': 'My User Agent 1.0'}).content))
     obj.save('gen_human.jpeg')
-    foto = cv2.imread('gen_human.jpeg')
+    foto = cv2.imread('gen_human.png')
     encoded = base64.b64encode(foto)
-    return ('<img src="data:image/png;base64,{0}"/>').format(encoded)
+    return ('data:image/png;base64,{0}"/>').format(encoded)
+
+make_human()
