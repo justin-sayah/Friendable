a0c5a85dbb1e83c57debaf81305918f8068a4d8c
PatKuz <pkuzdzal@bu.edu>
2021-11-14 06:10:17 -0500

    dis da big change :O

--- csv-to-firebase.py
+++ csv-to-firebase.py
@@ -13,7 +13,7 @@ def i_love_data(filename):
                 line = line.replace('\n','').split(',')
                 num = PhoneNumber('US').get_number()
                 num = str(num[2:])
-                vals = {'Name': line[0],'EF':float(line[1]),'SI':float(line[2]),'TF':float(line[3]),'JP':float(line[4]),'Cereal':int(line[5]),'Hotdog':int(line[6]),'Sleep':int(line[7]),'IceCream':int(line[8]),'Messy':int(line[9]),'Aliens':int(line[10]),'Classes':int(line[11]), 'Phone':num}
+                vals = {'Name': line[0],'EF':float(line[1]),'SI':float(line[2]),'TF':float(line[3]),'JP':float(line[4]),'Cereal':int(line[5]),'Hotdog':int(line[6]),'Sleep':int(line[7]),'IceCream':int(line[8]),'Messy':int(line[9]),'Aliens':int(line[10]),'Classes':int(line[11]), 'number':num}
                 print('[*] inserting ' + line[0] + ' with id ' + num)
                 users.document(num).set(vals)
             except:
