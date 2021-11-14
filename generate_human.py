8828ee814570c5e7cc4f62c546f59950b057b95e
PatKuz <>
2021-11-14 03:24:41 -0500

    avatar generator maybe?

---
+++ generate_human.py
@@ -0,0 +1,9 @@
+import requests, io, base64, cv2
+import PIL.Image as img
+
+def make_human():
+    obj = img.open(io.BytesIO(requests.get("https://thispersondoesnotexist.com/image", headers={'User-Agent': 'My User Agent 1.0'}).content))
+    obj.save('gen_human.jpeg')
+    foto = cv2.imread('gen_human.jpeg')
+    encoded = base64.b64encode(foto)
+    return ('<img src="data:image/png;base64,{0}"/>').format(encoded)
