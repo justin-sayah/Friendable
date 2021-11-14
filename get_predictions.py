ec8c77db30a3477c5c7409abbebe282466b0f449
Justin Sayah <55928969+justin-sayah@users.noreply.github.com>
2021-11-14 00:14:54 -0500

    testing get_predictions

--- get_predictions.py
+++ get_predictions.py
@@ -3,31 +3,44 @@ import pandas as pd
 from sklearn.cluster import KMeans
 from joblib import dump, load
 
-def get_prediction(value_arr):
+def get_prediction(value_dict):
     '''
     Gets a prediction from in house trained kmeans model
 
-    :param value_arr: an array of values matching the order in fake_people DO NOT PASS A NAME
+    :param value_arr: a dict of values matching the order in fake_people DO NOT PASS A NAME
     '''
-    kmeans = load('kmeans_model.joblib')
-    test = np.array(value_arr)
-    test.reshape(-1,1)
-    prediction = kmeans.predict([test])
-    return prediction[0]
-
-def process_data(value_dict):
-    '''
-    takes in a dictionary of values and returns an array ready to get passed to get_prediction
-
-    I know I need to average
-    '''
-    # {('number', '9788065553'), ('name', 'Conor Walsh'), ('q1', '7'), ('q2', '3'), ('q3', '9'), 
-    # ('q4', '2'), ('q5', '8'), ('q6', '8'), ('q7', '8'), ('q8', '8'), ('q9', '8'), ('q10', '3'), 
-    # ('q11', '6'), ('q12', '6'), ('13', '1'), ('14', '1'), ('15', '1'), ('16', '1'), ('17', '0'), ('18', '0')}
 
     preprocessed = list(value_dict.values())
     preprocessed = preprocessed[2:]
-    print(preprocessed)
+    
+    processed = []
+
+    for i in range(3):
+        sum = 0
+        sum += int(preprocessed[i])
+        processed.append(sum/3)
+    for i in range(3,6):
+        sum = 0
+        sum += int(preprocessed[i])
+        processed.append(sum/3)
+    for i in range(6,9):
+        sum = 0
+        sum += int(preprocessed[i])
+        processed.append(sum/3)
+    for i in range(9,12):
+        sum = 0
+        sum += int(preprocessed[i])
+        processed.append(sum/3)
+
+    for i in range(13,19):
+        processed.append(int(preprocessed[i]))
+    
+    kmeans = load('kmeans_model.joblib')
+    test = np.array(processed)
+    test.reshape(-1,1)
+    prediction = kmeans.predict([test])
+
+    return prediction[0]
 
         
 
